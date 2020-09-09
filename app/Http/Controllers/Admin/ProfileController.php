15775713e9fa71b710566e9ab72073b5df6ab3ae
Pan <s-pengqin.pan@rakuten.com>
2020-09-09 17:21:02 +0900

    mynews

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -2,10 +2,29 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ProfileController extends Controller
 {
-    //
+    public function add()
+    {
+        return view('admin.profile.create');
+    }
+    
+    public function create()
+    {
+        return redirect('admin/profile/create');
+    }
+    
+    
+    public function edit()
+    {
+        return view('admin.profile.edit');
+    }
+    
+    public function update()
+    {
+        return redirect('admin/profile/edit');
+    }
 }
