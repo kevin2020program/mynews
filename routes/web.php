901486d297f03fb16f103337bfd3cfa58cfaab82
Pan <s-pengqin.pan@rakuten.com>
2020-09-14 13:45:03 +0900

    0914

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,21 @@ Route::group(['prefix' => 'admin'], function() {
 
 Route::get('XXX', 'AAAController@bbb');
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
